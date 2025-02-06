cc5d0f9a87847f68d27b88272f7abaf363592dd3
Dave Aspinall <dave.aspinall@cinch.co.uk>
2025-02-06 01:21:02 +0000

    chore: add aria label to no posts text emoji

--- src/components/posts-error/posts-error.tsx
+++ src/components/posts-error/posts-error.tsx
@@ -2,7 +2,9 @@ import * as styles from "./posts-error.module.css";
 
 export const PostsError = () => (
   <div className={styles.error}>
-    <span className={styles.emoji}>(&gt;_&lt;)</span>
+    <span className={styles.emoji} aria-label="Oh no!">
+      (&gt;_&lt;)
+    </span>
     <span className={styles.title}>Can't find any posts</span>
     <span>Clear your search and try again</span>
   </div>
