79bdb5c924f4d0d551d7695f16f0bb58dcdca5c3
Dave Aspinall <dave.aspinall@cinch.co.uk>
2025-02-11 13:42:11 +0000

    feat: add react-query-devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "./lib/query-client";
 
 import { Homepage } from "@/components/pages/home";
 import "./App.css";
+import { PaginationProvider } from "./hooks/use-pagination/use-pagination";
 
 export const App = () => (
   <QueryClientProvider client={queryClient}>
-    <Homepage />
+    <PaginationProvider>
+      <Homepage />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </PaginationProvider>
   </QueryClientProvider>
 );
